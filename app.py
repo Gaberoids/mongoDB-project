471c736903cac624e6ab3533e6de4db3785e1dd8
Gaberoids <figueiropas@hotmail.com>
2020-12-28 22:51:49 +0000

    commit after creating the .html template

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 import os
-from flask import Flask
+from flask import (Flask, flash,
+    render_template, redirect, request,
+    session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+# this will allow to use code to
+# ... access the data on docs
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+# building the connection
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# creating an instance of pymongo. constructor method
+# ... "app" is the same as the above app = Flask(...).
+mongo = PyMongo(app)
+
 
 @app.route("/")
+@app.route("/get_tasks")
 def hello():
     return "Hello World ... again!"
+
+
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    # .tasks. = collection
+    return render_template("tasks.html", tasks=tasks)
+    # tasks=tasks. first "task" = template .html will use
+    # ... second "tasks" is the var tasks = mongo.db.tasks.fin...
 
 
 if __name__ == "__main__":

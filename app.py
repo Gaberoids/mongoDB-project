836c1291123298f40b22a166e185e7e94761801d
Gaberoids <figueiropas@hotmail.com>
2020-12-28 23:31:53 +0000

    commit after creating the .html template 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import (Flask, flash,
-    render_template, redirect, request,
-    session, url_for)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 # this will allow to use code to

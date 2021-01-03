7aaa9b44a30b81200174606782dfd062d6f56faa
Gaberoids <figueiropas@hotmail.com>
2021-01-03 01:10:45 +0000

    Add Materialize Navbar and Sidenav

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// this code initialize the mobil version of nav page. It was taken from normalize css web page https://materializecss.com/navbar.html
+$(document).ready(function () {
+    $('.sidenav').sidenav({ edge: "right" }); // sidenav show on the right https://materializecss.com/sidenav.html#options
+});

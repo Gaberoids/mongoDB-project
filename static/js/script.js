a1aef72631b67bf86009964251b9f66c48ade250
Gaberoids <figueiropas@hotmail.com>
2021-01-08 00:52:58 +0000

    accordion/collapsable functionality added

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 // this code initialize the mobil version of nav page. It was taken from normalize css web page https://materializecss.com/navbar.html
 $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" }); // sidenav show on the right https://materializecss.com/sidenav.html#options
+    $('.collapsible').collapsible();  // this is for the acordedon collapsable items from materializecss too
 });
